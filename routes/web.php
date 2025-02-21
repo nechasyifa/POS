4ffdfb4bb8784cbcb19944e18c6d1e7f684afb29
nechasyifa <nechasyifasyafitri@gmail.com>
2025-02-22 03:59:23 +0700

    Praktikum Jobsheet2

---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\PenjualanController;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [HomeController::class, 'home']);
+
+Route::prefix('category')->group(function () {
+    Route::get('/food-beverage', [ProductController::class, 'foodbeverage']);
+    Route::get('/beauty-health', [ProductController::class, 'beautyhealth']);
+    Route::get('/home-care', [ProductController::class, 'homecare']);
+    Route::get('/baby-kid', [ProductController::class, 'babykid']);
+});
+
+Route::get('/user/{id}/name/{name}', [UserController::class, 'user']);
+
+Route::get('/penjualan', [PenjualanController::class, 'penjualan']);
